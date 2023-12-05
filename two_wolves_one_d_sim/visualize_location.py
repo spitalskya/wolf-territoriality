98ab955da14b3cd05113bc5e609ea0dd9f6161b8
spitalskya <spitalskya@gmail.com>
2023-12-05 07:43:26 +0100

    After sim

--- two_wolves_one_d_sim/visualize_location.py
+++ two_wolves_one_d_sim/visualize_location.py
@@ -3,14 +3,13 @@ import matplotlib.pyplot as plt
 
 simulations = pd.read_csv('two_wolves_one_d_sim/simulated_data/simulations.csv')
 
-idx = 1
+idx = 5
 file = simulations['Name'][idx]
 size = simulations['AreaSize'][idx]
 
 print(file)
 
 data = pd.read_csv(f'two_wolves_one_d_sim/simulated_data/{file}')
-#data =  pd.read_csv(f'two_wolves_one_d_sim/loc_end_murray_lewis_size_40.csv')
 plt.hist(data['A'], bins=size, range=(0, size), color='blue', alpha=0.7)
 plt.hist(data['B'], bins=size, range=(0, size), color='orange', alpha=0.7)
 plt.show() 
