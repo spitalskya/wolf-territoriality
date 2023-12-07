f28827a965505c35a9550e9fdd878ac95bb72430
spitalskya <spitalskya@gmail.com>
2023-12-07 10:10:21 +0100

    After sim commit

--- two_wolves_one_d_sim/visualize_location.py
+++ two_wolves_one_d_sim/visualize_location.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 simulations = pd.read_csv('two_wolves_one_d_sim/simulated_data/simulations.csv')
 
-idx = 6
+idx = 7
 file = simulations['Name'][idx]
 size = simulations['AreaSize'][idx]
 
@@ -12,4 +12,5 @@ print(file)
 data = pd.read_csv(f'two_wolves_one_d_sim/simulated_data/{file}')
 plt.hist(data['A'], bins=size, range=(0, size), color='blue', alpha=0.7)
 plt.hist(data['B'], bins=size, range=(0, size), color='orange', alpha=0.7)
+# plt.hist(data['C'], bins=size, range=(0, size), color='green', alpha=0.7)
 plt.show() 
