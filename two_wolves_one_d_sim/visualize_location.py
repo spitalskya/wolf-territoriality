f9a1fb591d052d9fd6f500a7a5050f9dbe07ab2a
spitalskya <spitalskya@gmail.com>
2023-12-07 11:08:25 +0100

    Generated histogram pngs

--- two_wolves_one_d_sim/visualize_location.py
+++ two_wolves_one_d_sim/visualize_location.py
@@ -12,5 +12,9 @@ print(file)
 data = pd.read_csv(f'two_wolves_one_d_sim/simulated_data/{file}')
 plt.hist(data['A'], bins=size, range=(0, size), color='blue', alpha=0.7)
 plt.hist(data['B'], bins=size, range=(0, size), color='orange', alpha=0.7)
-# plt.hist(data['C'], bins=size, range=(0, size), color='green', alpha=0.7)
-plt.show() 
+if idx == 7:
+    plt.hist(data['C'], bins=size, range=(0, size), color='green', alpha=0.7)
+
+plt.title(file[:-4])
+# plt.savefig(file[:-4] + '.png')
+
